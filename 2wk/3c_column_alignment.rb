55db08693be3d63ab5b61fc55e2bcb296bc57e37
sharang-d <sharang.d@gmail.com>
2013-07-09 01:58:33 +0530

    * Created add_line_number_prefixes() to return a string

--- 2wk/3c_column_alignment.rb
+++ 2wk/3c_column_alignment.rb
@@ -1,8 +1,13 @@
+def add_line_number_prefixes(s)
+  result = ''
+  str_array = s.lines
+  max_width = str_array.count.to_s.length
+  str_array.each.with_index(1) { |s, i|  result << "Line %#{max_width}d: #{s}" %i }
+  result
+end
+
 s = ''
 for i in 1..100_000
   s << i.to_s << "\n"
 end
-
-str_array = s.lines
-max_width = str_array.count.to_s.length
-str_array.each.with_index(1) { |s, i|  puts "Line %#{max_width}d: #{s}" %i }
+puts add_line_number_prefixes(s)
