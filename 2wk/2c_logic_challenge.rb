740d94a05e6d4b336436cb1fee6f7d9b8dc39cec
sharang-d <sharang.d@gmail.com>
2013-07-09 02:02:12 +0530

    * Changed default values. No change in the logic.

--- 2wk/2c_logic_challenge.rb
+++ 2wk/2c_logic_challenge.rb
@@ -1,7 +1,7 @@
-def conditional(condition, true_value = 'yeah', false_value = 'boo')
-   condition && true_value or false_value 
+def conditional(condition, true_value = 'true', false_value = 'false')
+  condition && true_value or false_value 
 end
 
-puts conditional(5 > 3)
-puts conditional(5 < 3)
-puts conditional(5 == 5, "yes", "no")
+puts conditional(5 > 3, "yeah", "boo")
+puts conditional(5 < 3, "yeah", "boo")
+puts conditional(5 == 5)
