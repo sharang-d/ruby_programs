1c8914231075a2979522a05797343aee8306e937
sharang-d <sharang.d@gmail.com>
2013-07-08 22:12:38 +0530

    * Changed Statement in leap_year?() * created minutes_in_year(year)

--- 2wk/3e_leap_or_not.rb
+++ 2wk/3e_leap_or_not.rb
@@ -1,8 +1,12 @@
 def leap_year?(year)
-  year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)
+  year % 400 == 0 or year % 4 == 0 && year % 100 != 0
+end
+
+def minutes_in_year(year)
+  60 * 24 * (leap_year?(year) ? 366 : 365)
 end
 
 [ 2000, 2004, 1900, 2005 ].each do |year|
   leap = leap_year?(year)
-  puts "Number of minutes in the #{'(leap)' if leap}year #{year}: #{60 * 24 * (leap ? 366 : 365)}"
+  puts "Number of minutes in the #{'(leap)' if leap}year #{year}: #{minutes_in_year(year)}"
 end
