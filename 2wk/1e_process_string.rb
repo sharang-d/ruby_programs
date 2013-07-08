db506cfa25d86b2b5508c6f0e2843e63b9bbc5f0
sharang-d <sharang.d@gmail.com>
2013-07-08 22:01:04 +0530

    Used String#lines

--- 2wk/1e_process_string.rb
+++ 2wk/1e_process_string.rb
@@ -1,4 +1,5 @@
 s = "Welcome to the forum.\nHere you can learn Ruby.\nAlong with other members.\n"
-str_array = s.each_line
+#str_array = s.each_line
 #str_array.count.times { |i| puts "Line #{i+1}: #{str_array[i]}" }
-str_array.each.with_index(1) { |s,i| puts "Line #{i}: #{s}"}
+#str_array.each.with_index(1) { |s,i| puts "Line #{i}: #{s}"}
+s.lines.each.with_index(1) { |s, i| puts "Line #{i}: #{s}" }
