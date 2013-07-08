68c22ffdcee541dc4b000f8842e26cc2db464c17
sharang-d <sharang.d@gmail.com>
2013-07-08 22:31:43 +0530

    * Changed minutes_in_year to skip finding out if year is leap or not if it's already known(passed as an argument)

--- 2wk/3e_leap_or_not.rb
+++ 2wk/3e_leap_or_not.rb
@@ -2,11 +2,12 @@ def leap_year?(year)
   year % 400 == 0 or year % 4 == 0 && year % 100 != 0
 end
 
-def minutes_in_year(year)
-  60 * 24 * (leap_year?(year) ? 366 : 365)
+def minutes_in_year(year, leap = -1)
+  leap == -1 && leap = leap_year?(year)
+  60 * 24 * (leap ? 366 : 365)
 end
 
 [ 2000, 2004, 1900, 2005 ].each do |year|
   leap = leap_year?(year)
-  puts "Number of minutes in the #{'(leap)' if leap}year #{year}: #{minutes_in_year(year)}"
+  puts "Number of minutes in the #{'(leap)' if leap}year #{year}: #{minutes_in_year(year, leap)}"
 end
