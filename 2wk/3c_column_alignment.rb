2a7db6827bc33eeb361a7847258833f4af33ca2c
sharang-d <sharang.d@gmail.com>
2013-07-09 01:34:25 +0530

    * Changed some methods of string manipulation

--- 2wk/3c_column_alignment.rb
+++ 2wk/3c_column_alignment.rb
@@ -1,8 +1,8 @@
 s = ''
-for i in 1..50_000
+for i in 1..100_000
   s << i.to_s << "\n"
 end
-str_array = s.split("\n")
-count = str_array.length
-max_width = count.to_s.length
-count.times { |count|  printf "Line %#{max_width}d: #{(str_array[count])}\n" %(count + 1) }
+
+str_array = s.lines
+max_width = str_array.count.to_s.length
+str_array.each.with_index(1) { |s, i|  puts "Line %#{max_width}d: #{s}" %i }
