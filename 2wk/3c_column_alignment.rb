951934d869ee43348c91f0974ebe22e902c7d39b
sharang-d <sharang.d@gmail.com>
2013-07-09 19:28:05 +0530

    * Used different string methods

--- 2wk/3c_column_alignment.rb
+++ 2wk/3c_column_alignment.rb
@@ -1,11 +1,11 @@
 def add_line_number_prefixes(s)
   str_array = s.lines
   max_width = str_array.count.to_s.length
-  str_array.map.with_index(1){ |s, i|  "Line %#{max_width}d: #{s}" % i }.join
+  str_array.map.with_index(1) { |s, i|  "Line %#{max_width}d: #{s}" % i }.join
 end
 
 s = ''
 for i in 1..100_000
   s << i.to_s << "\n"
 end
-puts add_line_number_prefixes(s)
+print add_line_number_prefixes(s)
