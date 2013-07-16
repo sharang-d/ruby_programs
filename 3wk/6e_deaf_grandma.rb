48acda2aa5cbde86e045210c2f073ba23186ac76
sharang-d <sharang.d@gmail.com>
2013-07-16 13:08:30 +0530

    Changed the loop

--- 3wk/6e_deaf_grandma.rb
+++ 3wk/6e_deaf_grandma.rb
@@ -1,11 +1,7 @@
 STDOUT.sync = true
-fail_phrase = 'UH?! SPEAK UP, SONNY!'
+fail_phrase = 'HUH?! SPEAK UP, SONNY!'
 pass_phrase = Proc.new { "NO, NOT SINCE #{rand(1930..1950)}!"}
 
-while true do
-  print "You: "
-  str = gets.chomp
-  break if str == 'BYE'
+while !(print "You: ") && (str = gets.chomp) != 'BYE'  do
   puts "Grandma: #{str.upcase == str ? pass_phrase.call : fail_phrase}"
 end
-
