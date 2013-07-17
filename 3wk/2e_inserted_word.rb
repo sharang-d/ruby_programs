ecc37276c77e3f682369d298abcad9c92ba2168b
sharang-d <sharang.d@gmail.com>
2013-07-17 13:40:50 +0530

    * Changed f1.print to f1.puts

--- 3wk/2e_inserted_word.rb
+++ 3wk/2e_inserted_word.rb
@@ -1,4 +1,4 @@
 file = ARGV[0] || 'raw_files/2e_text.txt'
 text = File.open(file) {|f1| f1.read}
 text.gsub!(/(^| )word($| )/) {|match| match.sub('word','inserted word')}
-File.open(file, 'w') {|f1| f1.print(text)}
+File.open(file, 'w') {|f1| f1.puts text}
