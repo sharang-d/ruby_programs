2278eeb6add3b3624344c501f580a67a5631b95e
sharang-d <sharang.d@gmail.com>
2013-07-17 19:36:01 +0530

    * Changed gsub statement. Used Regex groups.

--- 3wk/2e_inserted_word.rb
+++ 3wk/2e_inserted_word.rb
@@ -1,4 +1,4 @@
 file = ARGV[0] || 'raw_files/2e_text.txt'
 text = File.open(file) {|f1| f1.read}
-text.gsub!(/(^| )word($| )/) {|match| match.sub('word','inserted word')}
+text.gsub!(/(^| )word($| )/, '\1inserted word\2')
 File.open(file, 'w') {|f1| f1.puts text}
