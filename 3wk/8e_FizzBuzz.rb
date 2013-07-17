6dfd884646f77cb2b5fa2d9f899a94e838a4f2ea
sharang-d <sharang.d@gmail.com>
2013-07-17 14:16:56 +0530

    Week 3

--- 3wk/8e_FizzBuzz.rb
+++ 3wk/8e_FizzBuzz.rb
@@ -1,11 +1,9 @@
-numbers = (1..100)
+numbers = (1..100).to_a
 numbers.each_with_index do |number,index|
   result = ''
   number % 3 == 0 && result.concat('Fizz')
   number % 5 == 0 && result.concat('Buzz')
   result == '' || numbers[index] = result
-
-
 end
 
 p numbers
