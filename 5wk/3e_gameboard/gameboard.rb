f20fe917c1754903b82d8252d88e3c48081165a9
sharang-d <sharang.d@gmail.com>
2013-07-31 03:38:45 +0530

    Final submission

--- 5wk/3e_gameboard/gameboard.rb
+++ 5wk/3e_gameboard/gameboard.rb
@@ -1,16 +1,16 @@
 class GameBoard
-  def initialize
-  end
 
   def set_locations_cells loc
     @locations = loc
     @guessed = []
+    @no_of_hits = 0
   end
 
   def check_yourself num
     num = num.to_i
     if @locations.include?(num)
       @guessed << num unless @guessed.include?(num)
+      @no_of_hits += 1
       if @locations.all? {|e| @guessed.include?(e)} 
         puts 'End'
         return 'kill' 
@@ -21,4 +21,5 @@ class GameBoard
       puts 'Miss'
     end
   end
+
 end
