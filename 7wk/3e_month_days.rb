4b06b1e697186a3cc98f534ff506f462b8d53fff
sharang-d <sharang.d@gmail.com>
2013-08-18 20:28:31 +0530

    * Doctests

--- 7wk/3e_month_days.rb
+++ 7wk/3e_month_days.rb
@@ -7,10 +7,11 @@ DAYS_IN_MONTHS = [29, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 # >> month_days(2, 2013)
 # => 28
 def month_days(month, year = Time.now.year)
-  DAYS_IN_MONTHS[month == 2 && Date.gregorian_leap?(year) ? 0 : 2]
+  DAYS_IN_MONTHS[month == 2 && Date.gregorian_leap?(year) ? 0 : month]
 end
 
 if __FILE__ == $PROGRAM_NAME
   puts month_days 5
   puts month_days(2, 2000)
+  puts month_days 10
 end
