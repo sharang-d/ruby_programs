31f41f6ea7ff4a20c801a261856e5eb1be048fcf
sharang-d <sharang.d@gmail.com>
2013-08-18 20:44:49 +0530

    * Moved Notes to Vics Notes

--- benchmark_delete_at_slice.rb
+++
@@ -1,9 +0,0 @@
-require 'benchmark'
-
-array_size = 1000
-iterations = 10000
-
-Benchmark.bmbm do |x|
-  x.report('slice!') { iterations.times { (1..array_size).to_a.slice!(20)}}
-  x.report('delete_at') { iterations.times { (1..array_size).to_a.delete_at(20)} }
-end
