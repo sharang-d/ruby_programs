6411c8096e72a3d327c4fd26f9a706e142beb919
sharang-d <sharang.d@gmail.com>
2013-08-19 20:59:24 +0530

    * round() used and column width increased Used round() instead of truncating the unwanted decimal places Increased Number of stars

--- 7wk/2c_distribution_random.rb
+++ 7wk/2c_distribution_random.rb
@@ -1,17 +1,18 @@
 h = Hash.new(0)
 100_000.times do
   number = (rand - rand) * 10.0
-  one_decimal_place = (number.abs == number ? number.floor : number.ceil) / 10.0
+  #one_decimal_place = (number.abs == number ? number.floor : number.ceil) / 10.0
+  one_decimal_place = number.round / 10.0
   h[one_decimal_place] += 1
 end
 
 freq = {}
 h.sort.each { |k,v| freq[k] = v }
-puts "\n  Number #{' ' * 16}Relative Frequency#{' ' * 16}Actual Frequency"
-puts '-' * 75 
-divisor = freq.values.max / 50.0
+puts "\n  Number #{' ' * 30}Relative Frequency#{' ' * 30}Actual Frequency"
+puts '-' * 103 
+divisor = freq.values.max / 80.0
 freq.each do |k,v|
   rel_freq = v / divisor
-  stars = "#{'*' * rel_freq}#{' ' * (50 - rel_freq.floor)}"
+  stars = "#{'*' * rel_freq}#{' ' * (80 - rel_freq.floor)}"
   puts "%7s: #{stars} %10s" %[k,v]
 end
