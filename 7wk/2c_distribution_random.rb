536c82a60bcbb264b44b71eb7cf0e20d6d9e2b95
sharang-d <sharang.d@gmail.com>
2013-08-19 15:08:08 +0530

    * Visual aid

--- 7wk/2c_distribution_random.rb
+++ 7wk/2c_distribution_random.rb
@@ -1,10 +1,17 @@
 h = Hash.new(0)
 100_000.times do
-  r1 = rand(-1.0..1.0)
-  r2 = rand(-1.0..1.0)
-  number = (r1 - r2) * 10.0
+  number = (rand - rand) * 10.0
   one_decimal_place = (number.abs == number ? number.floor : number.ceil) / 10.0
   h[one_decimal_place] += 1
 end
 
-h.sort.each { |k,v| puts "Number of occurrences of #{k}: #{v}" }
+freq = {}
+h.sort.each { |k,v| freq[k] = v }
+puts "\n  Number #{' ' * 16}Relative Frequency#{' ' * 16}Actual Frequency"
+puts '-' * 75 
+divisor = freq.values.max / 50.0
+freq.each do |k,v|
+  rel_freq = v / divisor
+  stars = "#{'*' * rel_freq}#{' ' * (50 - rel_freq.floor)}"
+  puts "%7s: #{stars} %10s" %[k,v]
+end
