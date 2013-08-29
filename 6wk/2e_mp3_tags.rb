79248dc20aa0b17b7a1a9f11b7c0cc48b4434352
Victor Goff <keeperotphones@gmail.com>
2013-08-29 14:02:39 -0500

    80 column rule, and DRY the tag reads

--- 6wk/2e_mp3_tags.rb
+++ 6wk/2e_mp3_tags.rb
@@ -1,7 +1,29 @@
 class AudioTags
   attr_reader :details # for reading specific properties
 
-  @@genres = ['Blues','ClassicRock','Country','Dance','Disco','Funk','Grunge','Hip-Hop','Jazz','Metal','NewAge','Oldies','Other','Pop','R&B','Rap','Reggae','Rock','Techno','Industrial','Alternative','Ska','DeathMetal','Pranks','Soundtrack','Euro-Techno','Ambient','Trip-Hop','Vocal','Jazz+Funk','Fusion','Trance','Classical','Instrumental','Acid','House','Game','SoundClip','Gospel','Noise','AlternRock','Bass','Soul','Punk','Space','Meditative','InstrumentalPop','InstrumentalRock','Ethnic','Gothic','Darkwave','Techno-Industrial','Electronic','Pop-Folk','Eurodance','Dream','SouthernRock','Comedy','Cult','Gangsta','Top40','ChristianRap','Pop/Funk','Jungle','NativeAmerican','Cabaret','NewWave','Psychadelic','Rave','Showtunes','Trailer','Lo-Fi','Tribal','AcidPunk','AcidJazz','Polka','Retro','Musical','Rock&Roll','HardRock','Folk','Folk-Rock','NationalFolk','Swing','FastFusion','Bebob','Latin','Revival','Celtic','Bluegrass','Avantgarde','GothicRock','ProgressiveRock','PsychedelicRock','SymphonicRock','SlowRock','BigBand','Chorus','EasyListening','Acoustic','Humour','Speech','Chanson','Opera','ChamberMusic','Sonata','Symphony','BootyBass','Primus','PornGroove','Satire','SlowJam','Club','Tango','Samba','Folklore','Ballad','PowerBallad','RhythmicSoul','Freestyle','Duet','PunkRock','DrumSolo','Acapella','Euro-House','DanceHall']
+  @@genres = [
+    'Blues', 'ClassicRock', 'Country', 'Dance', 'Disco', 'Funk', 'Grunge',
+    'Hip-Hop', 'Jazz', 'Metal', 'NewAge', 'Oldies', 'Other', 'Pop', 'R&B',
+    'Rap', 'Reggae', 'Rock', 'Techno', 'Industrial', 'Alternative', 'Ska',
+    'DeathMetal', 'Pranks', 'Soundtrack', 'Euro-Techno', 'Ambient', 'Trip-Hop',
+    'Vocal', 'Jazz+Funk', 'Fusion', 'Trance', 'Classical', 'Instrumental',
+    'Acid', 'House', 'Game', 'SoundClip', 'Gospel', 'Noise', 'AlternRock',
+    'Bass', 'Soul', 'Punk', 'Space', 'Meditative', 'InstrumentalPop',
+    'InstrumentalRock', 'Ethnic', 'Gothic', 'Darkwave', 'Techno-Industrial',
+    'Electronic', 'Pop-Folk', 'Eurodance', 'Dream', 'SouthernRock', 'Comedy',
+    'Cult', 'Gangsta', 'Top40', 'ChristianRap', 'Pop/Funk', 'Jungle',
+    'NativeAmerican', 'Cabaret', 'NewWave', 'Psychadelic', 'Rave', 'Showtunes',
+    'Trailer', 'Lo-Fi', 'Tribal', 'AcidPunk', 'AcidJazz', 'Polka', 'Retro',
+    'Musical', 'Rock&Roll', 'HardRock', 'Folk', 'Folk-Rock', 'NationalFolk',
+    'Swing', 'FastFusion', 'Bebob', 'Latin', 'Revival', 'Celtic', 'Bluegrass',
+    'Avantgarde', 'GothicRock', 'ProgressiveRock', 'PsychedelicRock',
+    'SymphonicRock', 'SlowRock', 'BigBand', 'Chorus', 'EasyListening',
+    'Acoustic', 'Humour', 'Speech', 'Chanson', 'Opera', 'ChamberMusic',
+    'Sonata', 'Symphony', 'BootyBass', 'Primus', 'PornGroove', 'Satire',
+    'SlowJam', 'Club', 'Tango', 'Samba', 'Folklore', 'Ballad', 'PowerBallad',
+    'RhythmicSoul', 'Freestyle', 'Duet', 'PunkRock', 'DrumSolo', 'Acapella',
+    'Euro-House', 'DanceHall',
+  ]
 
   def initialize file
     raise "#{file} does not exist." unless File.exist?(file)
@@ -14,11 +36,11 @@ class AudioTags
       f.seek(-128, IO::SEEK_END)
       if f.read(3) == 'TAG'
         @details[:tag] = 'Valid ID3 tag'
-        @details[:title] = f.read(30)
-        @details[:artist] = f.read(30)
-        @details[:album] = f.read(30)
-        @details[:year] = f.read(4)
-        @details[:comment] = f.read(30)
+        [
+          [:title, 30], [:artist, 30], [:album, 30], [:year, 4], [:comment, 30],
+        ].each do |tag, length|
+          @details[tag] = f.read(length)
+        end
         @details[:genre] = @@genres.fetch(f.read(1).unpack('C')[0], 'Unknown')
       else
         @details[:tag] = 'ID3 Tag not present'
@@ -26,8 +48,9 @@ class AudioTags
     end
   end
 
-  def to_s
-    @details.map { |k, v| "#{k}: #{v}"}.join("\n")
+  def to_s 
+    # the spirit of communicating a little more than what technically happens
+    @details.map { |tag, data | "#{tag}: #{data}"}.join("\n")
   end
 
 end
